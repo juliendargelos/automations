b916a51b39d81c072ca82459a67cc6db71a536a7
Julien Dargelos <julien.dargelos@me.com>
2022-07-09 16:59:23 +0200

    feat: test supabase api

--- api/youtube-playlists-filter.ts
+++ api/youtube-playlists-filter.ts
@@ -1,5 +1,23 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { createClient } from '@supabase/supabase-js'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  response.send('hey')
+  const supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_KEY!
+  )
+
+  const { data, error } = await supabase
+    .from('tokens')
+    .select()
+    .eq('service', 'youtube')
+    .limit(1)
+
+  const [{ token = null } = {}] = data || []
+
+  if (error || !token) {
+    response.send('not found')
+  } else {
+    response.send(token.test)
+  }
 }
