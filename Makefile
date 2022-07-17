a1480c0071281dee5eafcf9d1e9e893e7c56ed27
Julien Dargelos <julien.dargelos@me.com>
2022-07-17 14:38:27 +0200

    feat: wip

--- Makefile
+++ Makefile
@@ -1,22 +1,41 @@
+export DENO_DIR := .deno
+export BIN_DIR := bin
+export DENON := https://deno.land/x/denon@2.5.0/denon.ts
+export YTDLP := https://github.com/yt-dlp/yt-dlp/releases/download/2022.06.29/yt-dlp
+
 install:
-	@deno cache \
+	deno cache \
 		--lock-write \
 		--lock=lock.json \
 		**/*.ts
+
+	deno cache \
+		"$$DENON" \
+		--lock-write \
+		--lock=lock.json \
+		**/*.ts
+
+	mkdir -p "$$BIN_DIR"
+
+	curl \
+		-L "$$YTDLP" \
+		-o "$$BIN_DIR/yt-dlp"
+
+	chmod a+rx "$$BIN_DIR/yt-dlp"
 
 dev:
 	@deno run \
 		--allow-read \
 		--allow-write \
 		--allow-run \
-		https://deno.land/x/denon/denon.ts \
+		"$$DENON" \
 		deno check \
 		--lock=lock.json \
 		./common/*.ts \
 		./automations/**/*.ts
 
 %:
-	@deno run \
+	deno run \
 		--no-check \
 		--allow-net \
 		--allow-env \
---
+++ automations/youtube/clean-playlists.ts
@@ -0,0 +1,114 @@
+import { fetch } from '~/common/client.ts'
+import { authenticate } from '~/common/google.ts'
+import { notify } from '~/common/chanify.ts'
+
+import {
+  PLAYLIST_ITEMS_ENDPOINT,
+  VIDEOS_ENDPOINT,
+  MUSIC_CATEGORY
+} from '~/common/youtube.ts'
+
+// Api units used per run:
+// minimum: 20
+// maximum: 1020
+
+const ITEMS_COUNT = 50
+const PLAYLIST_IDS = [
+  'PLKtcW8LnNHduc7f0vdGIhemtkkyTtZ70n',
+  'PLKtcW8LnNHdv7GhwhV5I5yS2qiOE5RZPX'
+]
+
+const headers = {
+  Authorization: `Bearer ${await authenticate()}`,
+}
+
+console.log('Fetching youtube playlists...')
+
+const items = {  }
+
+const { items: playlistItems } = await fetch(PLAYLIST_ITEMS_ENDPOINT, {
+  headers,
+  params: {
+    part: 'snippet',
+    maxResults: ITEMS_COUNT,
+    playlistId: WATCH_LATER_PLAYLIST_ID
+  }
+})
+
+const { items: videos } = await fetch(VIDEOS_ENDPOINT, {
+  headers,
+  params: {
+    part: 'snippet',
+    id: playlistItems
+      .map((item: any) => item.snippet.resourceId.videoId)
+      .join(','),
+  }
+})
+
+console.log('Filtering videos...')
+
+let index: number
+let video: any
+let playlistItem: any
+let count: number = 0
+
+for (let index = 0; index < videos.length; index++) {
+  video = videos[index]
+  playlistItem = playlistItems[index]
+
+  if ((
+    // ignore videos that are not in the music category
+    !video.snippet.categoryId === MUSIC_CATEGORY ||
+
+    // ignore videos with titles that does look like song titles
+    !/\b(?: - |feat\.|edit|remix|mix|single|album|ep)\b/i
+      .test(video.snippet.title)
+  ) && (
+    // include any video with title strongly suggesting it is a song
+    !/\b(?:remix|full album|original mix|extended mix|full mix|acid mix)\b/i
+      .test(video.snippet.title)
+  )) {
+    continue
+  }
+
+  await fetch(PLAYLIST_ITEMS_ENDPOINT, {
+    headers,
+    unwrap: false,
+    method: 'delete',
+    params: {
+      id: playlistItems[index].id
+    }
+  })
+
+  await fetch(PLAYLIST_ITEMS_ENDPOINT, {
+    headers,
+    unwrap: false,
+    method: 'post',
+    params: {
+      part: 'snippet'
+    },
+    body: {
+      snippet: {
+        playlistId: LISTEN_LATER_PLAYLIST_ID,
+        resourceId: {
+          kind: 'youtube#video',
+          videoId: video.id
+        }
+      }
+    }
+  })
+
+  count++
+}
+
+const message = `${count} youtube video${count > 1
+  ? 's have'
+  : ' has'
+} been moved to listen later playlist`
+
+console.log(message)
+
+await count && notify({
+  title: 'Playlist filters',
+  text: message
+})
